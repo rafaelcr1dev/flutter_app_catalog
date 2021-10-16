740c2f1c3a35df9dd5f2cd98901dca9a27994965
rafaelcr123 <rafaelcruz1@hotmail.com>
2021-10-15 21:16:53 -0300

    Day 25 - Bad Code To The Rescue

--- lib/pages/home_detail_page.dart
+++ lib/pages/home_detail_page.dart
@@ -1,4 +1,5 @@
 import 'package:app_catalog/models/catalog.dart';
+import 'package:app_catalog/widgets/home_widgets/add_to_cart.dart';
 import 'package:flutter/material.dart';
 import 'package:velocity_x/velocity_x.dart';
 
@@ -23,7 +24,7 @@ class HomeDetailPage extends StatelessWidget {
           buttonPadding: Vx.mH8,
           children: [
             "\$${catalog.price}".text.bold.xl4.green800.make(),
-            _AddToCart().wh(120, 50),
+            AddToCart(catalog: catalog).wh(120, 50),
           ],
         ).p16(),
       ),
@@ -69,38 +70,6 @@ class HomeDetailPage extends StatelessWidget {
           ],
         ),
       ),
-    );
-  }
-}
-
-class _AddToCart extends StatefulWidget {
-  const _AddToCart({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  __AddToCartState createState() => __AddToCartState();
-}
-
-class __AddToCartState extends State<_AddToCart> {
-  bool isAdded = false;
-
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {
-        isAdded = isAdded.toggle();
-        setState(() {});
-      },
-      style: ButtonStyle(
-        backgroundColor: MaterialStateProperty.all(
-          context.theme.buttonColor,
-        ),
-        shape: MaterialStateProperty.all(
-          StadiumBorder(),
-        ),
-      ),
-      child: isAdded ? Icon(Icons.done) : "Add to cart".text.make(),
     );
   }
 }
--- lib/widgets/home_widgets/catalog_list.dart
+++ lib/widgets/home_widgets/catalog_list.dart
@@ -1,29 +1,32 @@
+import 'package:app_catalog/models/cart.dart';
 import 'package:app_catalog/models/catalog.dart';
 import 'package:app_catalog/pages/home_detail_page.dart';
-import 'package:app_catalog/utils/routes.dart';
 import 'catalog_item.dart';
 import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class CatalogList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      shrinkWrap: true,
-      itemCount: CatalogModel.items.length,
-      itemBuilder: (context, index) {
-        final catalog = CatalogModel.items[index];
-        return InkWell(
-          onTap: () => Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => HomeDetailPage(
-                catalog: catalog,
-              ),
-            ),
-          ),
-          child: CatalogItem(catalog: catalog),
-        );
-      },
-    );
+    return CatalogModel.items?.isEmpty
+        ? "Nothing to show".text.xl3.makeCentered()
+        : ListView.builder(
+            shrinkWrap: true,
+            itemCount: CatalogModel.items.length,
+            itemBuilder: (context, index) {
+              final catalog = CatalogModel.items[index];
+              return InkWell(
+                onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => HomeDetailPage(
+                      catalog: catalog,
+                    ),
+                  ),
+                ),
+                child: CatalogItem(catalog: catalog),
+              );
+            },
+          );
   }
 }
