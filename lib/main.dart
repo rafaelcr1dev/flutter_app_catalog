7723c92ee8be200146dcb9cb98bbb0543e28c48e
rafaelcr123 <rafaelcruz1@hotmail.com>
2021-10-15 21:31:11 -0300

    VelocityX State Management | VxState | VxStore

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
+import 'package:app_catalog/core/store.dart';
 import 'package:app_catalog/pages/cart_page.dart';
 import 'package:app_catalog/pages/home_page.dart';
 import 'package:app_catalog/pages/login_page.dart';
 import 'package:app_catalog/utils/routes.dart';
 import 'package:app_catalog/widgets/themes.dart';
 import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    VxState(
+      store: MyStore(),
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
